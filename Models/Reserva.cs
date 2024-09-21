26153a6286e35d9b58c7ff722aa36398ef6852fb
dineisruiz <82009616+dineisruiz@users.noreply.github.com>
2024-09-20 23:50:07 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,65 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+           
+            if (hospedes.Count >= Suite.Capacidade )
+            {
+                Hospedes = hospedes;
+                
+                
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+                throw new Exception("passou a capacidade permitida");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // Implementado
+            decimal valor = 0;
+            valor = Suite.ValorDiaria * DiasReservados;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // implementado
+            if (Suite.ValorDiaria >=10)
+            {
+                valor = valor * 0.9M;
+            }
+
+            return valor;
+        }
+    }
+}
